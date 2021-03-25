ed09c796b9ab2faff6be134da5900eea5f432523
Dylan Sapienza <dylsapienza12@gmail.com>
2021-03-24 20:31:38 -0400

    Added Sample Data

--- Schema.sql
+++ Schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE `Playlist` (
     PRIMARY KEY(`Playlist_ID`)
 );
 
-CREATE TABLE `InPlaylist`(
+CREATE TABLE `In_Playlist`(
     `Entry_ID` INT(9) NOT NULL AUTO_INCREMENT,
     `Item_ID` INT(9) NOT NULL,
     `Playlist_ID` VARCHAR(128) NOT NULL,
@@ -50,3 +50,43 @@ CREATE TABLE `Votes` (
     FOREIGN KEY (`Entry_ID`) REFERENCES In_Playlist(`Entry_ID`),
     FOREIGN KEY (`User_ID`) REFERENCES User(`User_ID`)
 );
+
+--Sample Data
+
+INSERT INTO User VALUES("1250899172", "dylsapienza12", "pass123");
+INSERT INTO User VALUES("bevans", "billevans7", "jazz251");
+INSERT INTO User VALUES("efitz", "ellafitzgerald", "sing123");
+INSERT INTO User VALUES("jrmat20", "jrmat20", "whimper20");
+
+INSERT INTO Song_Item VALUES(1, "spotify:track:7MAibcTli4IisCtbHKrGMh", "Leave The Door Open", "Silk Sonic", "An Evening with Silk Sonic", "");
+INSERT INTO Song_Item VALUES(2, "spotify:track:2Itnx6zp6Ztkex5JnRcy3U", "Bodies - Intro", "Jazmine Sullivan", "Heaux Tales", "");
+INSERT INTO Song_Item VALUES(3, "spotify:track:4ejQlM2w2X2AJqWYcMyNVm", "Heaven", "Pink Sweat", "PINK PLANET", "");
+INSERT INTO Song_Item VALUES(4, "spotify:track:5qAqBkA9qT0kpV1m8Sccc4", "Peri's Scope", "Bill Evans", "Portrait In Jazz", "");
+INSERT INTO Song_Item VALUES(5, "spotify:track:2d64G7VaZdHQuAquz5HQNu", "That Old Feeling", "Chet Baker", "Chet Baker Sings", "");
+INSERT INTO Song_Item Values(6, "spotify:track:5snRg0trIQBltwJZhVGlxr", "In Walked Bud", "Thelonious Monk", "Underground (Special Edition)", "")
+
+
+INSERT INTO Playlist VALUES("spotify:playlist:1Nzv5B0KGgqyo1I53YN0Pk", "Jazz");
+INSERT INTO Playlist VALUES("spotify:playlist:7tuTpeNJqJPDCTZrNkEVOv", "Neo-Soul");
+
+INSERT INTO In_Playlist VALUES(1, 1, "spotify:playlist:1Nzv5B0KGgqyo1I53YN0Pk", 0);
+INSERT INTO In_Playlist VALUES(2, 2, "spotify:playlist:1Nzv5B0KGgqyo1I53YN0Pk", 0);
+INSERT INTO In_Playlist VALUES(3, 3, "spotify:playlist:1Nzv5B0KGgqyo1I53YN0Pk", 0);
+INSERT INTO In_Playlist VALUES(4, 4, "spotify:playlist:7tuTpeNJqJPDCTZrNkEVOv", 0);
+INSERT INTO In_Playlist VALUES(5, 5, "spotify:playlist:7tuTpeNJqJPDCTZrNkEVOv", 0);
+INSERT INTO In_Playlist VALUES(6, 6, "spotify:playlist:7tuTpeNJqJPDCTZrNkEVOv", 0);
+INSERT INTO In_Playlist VALUES(7, 2, "spotify:playlist:7tuTpeNJqJPDCTZrNkEVOv", 0);
+
+INSERT INTO Votes VALUES(1, 2, '1250899172', 1);
+INSERT INTO Votes VALUES(2, 7, '1250899172', -1);
+INSERT INTO Votes VALUES(3, 5, '1250899172', 1);
+INSERT INTO Votes VALUES(4, 3, '1250899172', 0);
+INSERT INTO Votes VALUES(5, 4, 'efitz', 1);
+INSERT INTO Votes VALUES(6, 5, 'efitz', 1);
+INSERT INTO Votes VALUES(7, 6, 'efitz', 1);
+INSERT INTO Votes VALUES(8, 2, 'bevans', 1);
+INSERT INTO Votes VALUES(9, 1, 'bevans', 1);
+INSERT INTO Votes VALUES(10, 3, 'bevans', -1);
+INSERT INTO Votes VALUES(11, 1, 'jrmat20', 1);
+INSERT INTO Votes VALUES(12, 2, 'jrmat20', 1);
+INSERT INTO Votes VALUES(13, 5, 'jrmat20', 1);
