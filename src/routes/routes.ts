c0af2e86f10a604ddcb352b8e980a74ac16aa0af
Dylan Sapienza <dylsapienza12@gmail.com>
2021-03-27 14:15:17 -0400

    Fixed Router Typing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,6 @@
 import express = require("express");
 
-const router = express.Router;
+const router = express.Router();
 const controllers = require("../controllers/controllers");
 
 router.get("/hello_world", controllers.hello_world);
