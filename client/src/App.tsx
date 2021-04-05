3e765f130d770f2958f8e4ce8b37b87f9349e68d
Dylan Sapienza <dylsapienza12@gmail.com>
2021-04-05 15:16:26 -0400

    Added Login Form with Chakra

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,73 @@
-import * as React from "react"
 import {
-  ChakraProvider,
   Box,
+  Button,
+  Heading,
+  SimpleGrid,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  useColorModeValue as mode,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { Logo } from "./Logo";
+import { LoginForm } from "./components/LoginForm";
+import { FaFacebook, FaGoogle, FaGithub } from "react-icons/fa";
+import { DividerWithText } from "./components/DividerWithText";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+export const App = () => {
+  return (
+    <Box
+      bg={mode("gray.50", "inherit")}
+      minH="100vh"
+      py="12"
+      px={{ sm: "6", lg: "8" }}
+    >
+      <Box maxW={{ sm: "md" }} mx={{ sm: "auto" }} w={{ sm: "full" }}>
+        <Box mb={{ base: "10", md: "28" }}>
+          <Logo mx="auto" h="8" iconColor={mode("blue.600", "blue.200")} />
+        </Box>
+        <Heading mt="6" textAlign="center" size="xl" fontWeight="extrabold">
+          Sign in to your account
+        </Heading>
+        <Text mt="4" align="center" maxW="md" fontWeight="medium">
+          <span>Don&apos;t have an account?</span>
+          <Box
+            as="a"
+            marginStart="1"
+            href="#"
+            color={mode("blue.600", "blue.200")}
+            _hover={{ color: "blue.600" }}
+            display={{ base: "block", sm: "revert" }}
           >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+            Start free trial
+          </Box>
+        </Text>
+      </Box>
+      <Box maxW={{ sm: "md" }} mx={{ sm: "auto" }} mt="8" w={{ sm: "full" }}>
+        <Box
+          bg={mode("white", "gray.700")}
+          py="8"
+          px={{ base: "4", md: "10" }}
+          shadow="base"
+          rounded={{ sm: "lg" }}
+        >
+          <LoginForm />
+          <DividerWithText mt="6">or continue with</DividerWithText>
+          <SimpleGrid mt="6" columns={3} spacing="3">
+            <Button color="currentColor" variant="outline">
+              <VisuallyHidden>Login with Facebook</VisuallyHidden>
+              <FaFacebook />
+            </Button>
+            <Button color="currentColor" variant="outline">
+              <VisuallyHidden>Login with Google</VisuallyHidden>
+              <FaGoogle />
+            </Button>
+            <Button color="currentColor" variant="outline">
+              <VisuallyHidden>Login with Github</VisuallyHidden>
+              <FaGithub />
+            </Button>
+          </SimpleGrid>
+        </Box>
+      </Box>
     </Box>
-  </ChakraProvider>
-)
+  );
+};
